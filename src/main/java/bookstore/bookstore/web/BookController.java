225d0266a5fc585364fd12ad65c86a6d9d086e4e
kkivilahti <bhm585@myy.haaga-helia.fi>
2025-02-02 20:55:37 +0200

    small updates

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -2,10 +2,8 @@ package bookstore.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class BookController {
     @GetMapping("/index")
     public String index() {
